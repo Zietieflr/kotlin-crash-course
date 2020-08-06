6cea70e33bbc0d4d25cee487c7fa4d6936f70470
Zietieflr <lrmcguire93@gmail.com>
2020-08-06 11:36:49 -0600

    Tutorial functional to 1:00:00 mark.

--- app/src/main/java/com/tutorials/tutorial1/MainActivity.kt
+++ app/src/main/java/com/tutorials/tutorial1/MainActivity.kt
@@ -2,10 +2,19 @@ package com.tutorials.tutorial1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var timesClicked = 0
+        button.setOnClickListener {
+            timesClicked += 1
+            textView.text = timesClicked.toString()
+                Toast.makeText(this@MainActivity, "Clicked!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
